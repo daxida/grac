10a1cc539751ee2a590f380b31603cdffd8f6af4
daxida <galmaw@hotmail.com>
2024-11-24 22:35:04 +0100

    Add quickcheck test

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,5 +1,6 @@
 use grac::{is_diphthong, syllabify, syllabify_2, syllabify_3};
 use paste::paste;
+use quickcheck::quickcheck;
 
 #[test]
 fn test_is_diphthong() {
@@ -53,3 +54,37 @@ mktest!("english", vec!["english"]);
 mktest!("Ἀχαιιά", vec!["Ἀ", "χαι", "ι", "ά"]);
 mktest!("Ἠελίοιο", vec!["Ἠ", "ε", "λί", "οι", "ο"]);
 mktest!("Θρήικι", vec!["Θρή", "ι", "κι"]);
+
+const GREEK_LETTERS: &[(u32, u32)] = &[
+    (0x0370, 0x03FF), // Basic Greek and Coptic
+    (0x1F00, 0x1FFF), // Greek Extended
+];
+
+#[derive(Debug, Clone)]
+struct GreekWord(String);
+
+impl quickcheck::Arbitrary for GreekWord {
+    fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+        let wlen = usize::arbitrary(g) % 200 + 1;
+        let mut word = String::new();
+        let letters: Vec<char> = GREEK_LETTERS
+            .iter()
+            .flat_map(|&(start, end)| (start..=end))
+            .filter_map(char::from_u32)
+            .collect();
+        for _ in 0..wlen {
+            let c = g.choose(&letters).unwrap();
+            word.push(*c);
+        }
+        Self(word)
+    }
+}
+
+quickcheck! {
+    fn test_syllabify_equality(word: GreekWord) -> bool {
+        let result_1 = syllabify(&word.0);
+        let result_2 = syllabify_2(&word.0);
+        let result_3 = syllabify_3(&word.0);
+        result_1 == result_2 && result_2 == result_3
+    }
+}
