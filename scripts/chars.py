e5510aa89a8ceeb7a6cebe2a9e3e180f0608db1e
daxida <galmaw@hotmail.com>
2024-12-01 08:58:13 +0100

    Only normalize greek char range for efficiency

---
+++ scripts/chars.py
@@ -0,0 +1,57 @@
+import unicodedata
+
+
+def generate_rust_function(
+    fn_name: str = "base_lower_gc",
+    ignore_coptic: bool = True,
+    ignore_punct: bool = False,
+    to_lowercase: bool = True,
+) -> str:
+    if not ignore_coptic:
+        greek_and_coptic = range(0x0370, 0x03FF + 1)
+    else:
+        greek_and_coptic = range(0x0370, 0x03CF)
+    greek_extended = range(0x1F00, 0x1FFF + 1)
+
+    base_mapping = {}
+
+    for codepoint in greek_and_coptic:
+        char = chr(codepoint)
+
+        if ignore_punct and not unicodedata.category(char).startswith("L"):
+            continue
+
+        decomposed = unicodedata.normalize("NFD", char)
+        base_char = decomposed[0]
+        if to_lowercase:
+            base_char = base_char.lower()
+
+        # Skip characters that are their own base
+        if base_char != char:
+            if base_char not in base_mapping:
+                base_mapping[base_char] = []
+            base_mapping[base_char].append(char)
+
+    # Sorting for efficiency: lowercase, then uppercase, then others
+    base_mapping = dict(
+        sorted(
+            base_mapping.items(),
+            key=lambda p: (
+                not unicodedata.category(p[0]).startswith("L"),
+                not p[0].islower(),
+                p[0],
+            ),
+        )
+    )
+
+    code = f"fn {fn_name}" "(ch: char) -> char {\n    match ch {\n"
+    for base, chars in base_mapping.items():
+        variations = " | ".join(f"'{ch}'" for ch in chars)
+        code += f"        {variations} => '{base}',\n"
+    code += "        _ => ch,\n    }\n}\n"
+
+    return code
+
+
+if __name__ == "__main__":
+    print(generate_rust_function())
--- src/chars.rs
+++ src/chars.rs
@@ -2,13 +2,20 @@ use unicode_normalization::char::decompose_canonical;
 
 // TODO: Do stemming instead of using unicode_normalization
 
-// Doing this instead of ranges allows for const, but does not account for char gaps.
-const fn is_modern_lower_greek(ch: char) -> bool {
-    ch >= '\u{03B1}' && ch <= '\u{03C9}'
+const fn is_greek_and_coptic_char(ch: char) -> bool {
+    ch >= '\u{0370}' && ch <= '\u{03FF}'
 }
 
-// Faster than an .nfd() call for a single char.
-pub fn base(ch: char) -> char {
+pub const fn is_greek_extended_char(ch: char) -> bool {
+    ch >= '\u{1F00}' && ch <= '\u{1FFF}'
+}
+
+pub const fn is_greek_char(ch: char) -> bool {
+    is_greek_and_coptic_char(ch) || is_greek_extended_char(ch)
+}
+
+/// Oracle implementation for testing. Return the normalized character.
+fn __base(ch: char) -> char {
     let mut base_char = None;
     decompose_canonical(ch, |c| {
         base_char.get_or_insert(c);
@@ -16,11 +23,165 @@ pub fn base(ch: char) -> char {
     base_char.unwrap_or(ch)
 }
 
+/// Oracle implementation for testing. Return the normalized lower character.
+///
+/// This should be equal to [`base_lower`](self::base_lower)
+fn __base_lower(ch: char) -> char {
+    __base(ch).to_lowercase().next().unwrap_or(ch)
+}
+
+/// Return the normalized lower character.
 pub fn base_lower(ch: char) -> char {
-    if is_modern_lower_greek(ch) {
-        // Fast path for most of the chars
-        ch
-    } else {
-        base(ch).to_lowercase().next().unwrap_or(ch)
+    match ch {
+        // Greek and Coptic
+        '\u{0370}'..='\u{03FF}' => base_lower_gc(ch),
+        // Greek extended
+        '\u{1F00}'..='\u{1FFF}' => base_lower_ge(ch),
+        _ => ch,
+    }
+}
+
+/// Normalize and cast to lowercase Greek and Coptic: U+0370–U+03FF
+///
+/// This does not normalize:  ';' | '·' | 'Ϊ' | 'Ϋ' | 'ϓ' | 'ϔ'
+///
+/// https://www.unicode.org/charts/PDF/U0370.pdf
+fn base_lower_gc(ch: char) -> char {
+    match ch {
+        // Lowercase unaccented (α > ω)
+        // We want to check these first since they are most frequent.
+        '\u{03B1}'..='\u{03C9}' => ch,
+
+        // Lowercase accented vowels
+        'ά' => 'α',
+        'έ' => 'ε',
+        'ί' => 'ι',
+        'ή' => 'η',
+        'ό' => 'ο',
+        'ύ' => 'υ',
+        'ώ' => 'ω',
+
+        // Uppercase unaccented
+        'Α' => 'α',
+        'Β' => 'β',
+        'Γ' => 'γ',
+        'Δ' => 'δ',
+        'Ε' => 'ε',
+        'Ζ' => 'ζ',
+        'Η' => 'η',
+        'Θ' => 'θ',
+        'Ι' => 'ι',
+        'Κ' => 'κ',
+        'Λ' => 'λ',
+        'Μ' => 'μ',
+        'Ν' => 'ν',
+        'Ξ' => 'ξ',
+        'Ο' => 'ο',
+        'Π' => 'π',
+        'Ρ' => 'ρ',
+        'Σ' => 'σ',
+        'Τ' => 'τ',
+        'Υ' => 'υ',
+        'Φ' => 'φ',
+        'Χ' => 'χ',
+        'Ψ' => 'ψ',
+        'Ω' => 'ω',
+
+        // Uppercase accented vowels
+        'Ά' => 'α',
+        'Έ' => 'ε',
+        'Ί' => 'ι',
+        'Ή' => 'η',
+        'Ό' => 'ο',
+        'Ύ' => 'υ',
+        'Ώ' => 'ω',
+
+        // Diereses && punctuation
+        'ϊ' | 'ΐ' => 'ι',
+        'ϋ' | 'ΰ' => 'υ',
+        'ʹ' => 'ʹ',
+        '΅' => '¨',
+        _ => ch,
+    }
+}
+
+fn base_lower_ge(ch: char) -> char {
+    match ch {
+        'ἀ' | 'ἁ' | 'ἂ' | 'ἃ' | 'ἄ' | 'ἅ' | 'ἆ' | 'ἇ' | 'Ἀ' | 'Ἁ' | 'Ἂ' | 'Ἃ' | 'Ἄ' | 'Ἅ' | 'Ἆ'
+        | 'Ἇ' | 'ὰ' | 'ά' | 'ᾀ' | 'ᾁ' | 'ᾂ' | 'ᾃ' | 'ᾄ' | 'ᾅ' | 'ᾆ' | 'ᾇ' | 'ᾈ' | 'ᾉ' | 'ᾊ'
+        | 'ᾋ' | 'ᾌ' | 'ᾍ' | 'ᾎ' | 'ᾏ' | 'ᾰ' | 'ᾱ' | 'ᾲ' | 'ᾳ' | 'ᾴ' | 'ᾶ' | 'ᾷ' | 'Ᾰ' | 'Ᾱ'
+        | 'Ὰ' | 'Ά' | 'ᾼ' => 'α',
+        'ἐ' | 'ἑ' | 'ἒ' | 'ἓ' | 'ἔ' | 'ἕ' | 'Ἐ' | 'Ἑ' | 'Ἒ' | 'Ἓ' | 'Ἔ' | 'Ἕ' | 'ὲ' | 'έ' | 'Ὲ'
+        | 'Έ' => 'ε',
+        'ἠ' | 'ἡ' | 'ἢ' | 'ἣ' | 'ἤ' | 'ἥ' | 'ἦ' | 'ἧ' | 'Ἠ' | 'Ἡ' | 'Ἢ' | 'Ἣ' | 'Ἤ' | 'Ἥ' | 'Ἦ'
+        | 'Ἧ' | 'ὴ' | 'ή' | 'ᾐ' | 'ᾑ' | 'ᾒ' | 'ᾓ' | 'ᾔ' | 'ᾕ' | 'ᾖ' | 'ᾗ' | 'ᾘ' | 'ᾙ' | 'ᾚ'
+        | 'ᾛ' | 'ᾜ' | 'ᾝ' | 'ᾞ' | 'ᾟ' | 'ῂ' | 'ῃ' | 'ῄ' | 'ῆ' | 'ῇ' | 'Ὴ' | 'Ή' | 'ῌ' => {
+            'η'
+        }
+        'ἰ' | 'ἱ' | 'ἲ' | 'ἳ' | 'ἴ' | 'ἵ' | 'ἶ' | 'ἷ' | 'Ἰ' | 'Ἱ' | 'Ἲ' | 'Ἳ' | 'Ἴ' | 'Ἵ' | 'Ἶ'
+        | 'Ἷ' | 'ὶ' | 'ί' | 'ι' | 'ῐ' | 'ῑ' | 'ῒ' | 'ΐ' | 'ῖ' | 'ῗ' | 'Ῐ' | 'Ῑ' | 'Ὶ' | 'Ί' => {
+            'ι'
+        }
+        'ὀ' | 'ὁ' | 'ὂ' | 'ὃ' | 'ὄ' | 'ὅ' | 'Ὀ' | 'Ὁ' | 'Ὂ' | 'Ὃ' | 'Ὄ' | 'Ὅ' | 'ὸ' | 'ό' | 'Ὸ'
+        | 'Ό' => 'ο',
+        'ῤ' | 'ῥ' | 'Ῥ' => 'ρ',
+        'ὐ' | 'ὑ' | 'ὒ' | 'ὓ' | 'ὔ' | 'ὕ' | 'ὖ' | 'ὗ' | 'Ὑ' | 'Ὓ' | 'Ὕ' | 'Ὗ' | 'ὺ' | 'ύ' | 'ῠ'
+        | 'ῡ' | 'ῢ' | 'ΰ' | 'ῦ' | 'ῧ' | 'Ῠ' | 'Ῡ' | 'Ὺ' | 'Ύ' => 'υ',
+        'ὠ' | 'ὡ' | 'ὢ' | 'ὣ' | 'ὤ' | 'ὥ' | 'ὦ' | 'ὧ' | 'Ὠ' | 'Ὡ' | 'Ὢ' | 'Ὣ' | 'Ὤ' | 'Ὥ' | 'Ὦ'
+        | 'Ὧ' | 'ὼ' | 'ώ' | 'ᾠ' | 'ᾡ' | 'ᾢ' | 'ᾣ' | 'ᾤ' | 'ᾥ' | 'ᾦ' | 'ᾧ' | 'ᾨ' | 'ᾩ' | 'ᾪ'
+        | 'ᾫ' | 'ᾬ' | 'ᾭ' | 'ᾮ' | 'ᾯ' | 'ῲ' | 'ῳ' | 'ῴ' | 'ῶ' | 'ῷ' | 'Ὼ' | 'Ώ' | 'ῼ' => {
+            'ω'
+        }
+        '`' => '`',
+        '῁' | '῭' | '΅' => '¨',
+        '´' => '´',
+        '῍' | '῎' | '῏' => '᾿',
+        '῝' | '῞' | '῟' => '῾',
+        _ => ch,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn dbg_test(ch: char, received: char, expected: char) {
+        if received != expected {
+            println!(
+                "[DBG] Char: {}, Bytes: {:?}, received: {} ({:?}), expected: {} ({:?})",
+                ch,
+                ch.encode_utf8(&mut [0; 4]).as_bytes(),
+                received,
+                received.encode_utf8(&mut [0; 4]).as_bytes(),
+                expected,
+                expected.encode_utf8(&mut [0; 4]).as_bytes(),
+            );
+        }
+    }
+
+    #[test]
+    fn test_base_lower_greek_coptic() {
+        for ch in '\u{0370}'..='\u{03FF}' {
+            match ch {
+                ';' | '·' | 'Ϊ' | 'Ϋ' | 'ϓ' | 'ϔ' | 'Ͱ' | 'Ͳ' | 'ʹ' | 'Ͷ' | 'Ϳ' | 'Ϗ' | 'Ϙ'
+                | 'Ϝ' | 'Ϛ' | 'Ϟ' | 'Ϡ' | 'Ϣ' | 'Ϥ' | 'Ϧ' | 'Ϩ' | 'Ϫ' | 'Ϭ' | 'Ϯ' | 'ϴ' | 'Ϸ'
+                | 'Ϲ' | 'Ϻ' | 'Ͻ' | 'Ͼ' | 'Ͽ' => continue,
+                _ => (),
+            }
+            let expected = __base_lower(ch);
+            let received = base_lower_gc(ch);
+            dbg_test(ch, received, expected);
+            assert_eq!(received, expected);
+        }
+    }
+
+    #[test]
+    fn test_base_lower_greek_extended() {
+        for ch in '\u{1F00}'..='\u{1FFF}' {
+            let expected = __base_lower(ch);
+            let received = base_lower_ge(ch);
+            dbg_test(ch, received, expected);
+            assert_eq!(received, expected);
+        }
     }
 }
