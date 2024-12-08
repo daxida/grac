2f7588f5cf3212db5b5de392cf6e6833696ea66d
daxida <galmaw@hotmail.com>
2024-12-08 20:07:51 +0100

    Bench `syllabify_el`

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,7 +1,20 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use grac::{syllabify_gr, syllabify_gr_ref, to_mono};
+use grac::*;
 use std::fs::File;
 use std::io::{self, Read};
+
+macro_rules! benchmark_syllabify {
+    ($group:expr, $words:expr, $( $fn:ident ),* ) => {
+        $(
+            $group.bench_function(stringify!($fn), |b| {
+                b.iter(|| {
+                    let result: Vec<_> = $words.iter().map(|word| $fn(word)).collect();
+                    black_box(result);
+                });
+            });
+        )*
+    };
+}
 
 fn read_file(file_path: &str) -> io::Result<String> {
     let mut file = File::open(file_path)?;
@@ -10,26 +23,25 @@ fn read_file(file_path: &str) -> io::Result<String> {
     Ok(content)
 }
 
-pub fn benchmark_syllabify_file(c: &mut Criterion) {
+fn benchmark_syllabify(c: &mut Criterion) {
     let file_path = "dump.txt";
     let content = read_file(file_path).unwrap();
     let words: Vec<&str> = content.split_whitespace().collect();
 
     let mut group = c.benchmark_group("group");
-    group.sample_size(40);
-    group.bench_function("syllabify_gr", |b| {
-        b.iter(|| {
-            let result: Vec<_> = words.iter().map(|word| syllabify_gr(word)).collect();
-            black_box(result);
-        })
-    });
+    group.sample_size(20);
 
-    group.bench_function("syllabify_gr_ref", |b| {
-        b.iter(|| {
-            let result: Vec<_> = words.iter().map(|word| syllabify_gr_ref(word)).collect();
-            black_box(result);
-        })
-    });
+    benchmark_syllabify!(group, words, syllabify_gr);
+    benchmark_syllabify!(group, words, syllabify_gr_ref);
+    benchmark_syllabify!(group, words, syllabify_el);
+}
+
+fn benchmark_monotonic(c: &mut Criterion) {
+    let file_path = "dump.txt";
+    let content = read_file(file_path).unwrap();
+
+    let mut group = c.benchmark_group("group");
+    group.sample_size(10);
 
     group.bench_function("to_mono", |b| {
         b.iter(|| {
@@ -39,5 +51,5 @@ pub fn benchmark_syllabify_file(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, benchmark_syllabify_file);
+criterion_group!(benches, benchmark_syllabify, benchmark_monotonic);
 criterion_main!(benches);
