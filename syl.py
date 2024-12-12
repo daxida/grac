f0f9be9f0793002daec2b5150f1476bc4b3c6043
daxida <galmaw@hotmail.com>
2024-12-12 18:52:42 +0100

    Update testing script `syl.py`

--- syl.py
+++ syl.py
@@ -5,11 +5,17 @@ import grac
 
 import sys
 
-word = sys.argv[1]
-print(f"Ancient:      {gas.syllabify(word)}")
-print(f"Modern:       {mgas.modern_greek_syllabify(word)}")
-print(f"Monotonic:    {mgac.convert_to_monotonic(word)}")
+if len(sys.argv) > 1:
+    word = sys.argv[1]
+else:
+    word = "αστειάκια"
+
+print(f"Ancient:       {gas.syllabify(word)}")
+print(f"Modern:        {mgas.modern_greek_syllabify(word)}")
+print(f"Monotonic:     {mgac.convert_to_monotonic(word)}")
 print("Grac")
-print(f"Modern:       {grac.syllabify_el(word)}")
-print(f"Modern (syn): {grac.syllabify_el_syn(word)}")
-print(f"Monotonic:    {grac.to_mono(word)}")
+print(f"Modern:        {grac.syllabify_el(word)}")
+print(f"Modern (syn):  {grac.syllabify_el_mode(word, True)}")
+print(f"Modern (~syn): {grac.syllabify_el_mode(word, False)}")
+print(f"Modern (syn1): {grac.syllabify_el_mode_at(word, [1])}")
+print(f"Monotonic:     {grac.to_mono(word)}")
