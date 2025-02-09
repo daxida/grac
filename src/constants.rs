23d2dc5f0a8d8e0b25203125c79aa2f593fa43e2
daxida <galmaw@hotmail.com>
2025-02-09 23:09:08 +0100

    Add another apostrophe variant

--- src/constants.rs
+++ src/constants.rs
@@ -2,4 +2,4 @@
 ///
 /// Note that this does not include the dot or ellipsis, because, unlike
 /// those, apostrophes can only omit one letter.
-pub const APOSTROPHES: &[char] = &['᾽', '᾿', '\'', '‘'];
+pub const APOSTROPHES: &[char] = &['᾽', '᾿', '\'', '‘', '’'];
